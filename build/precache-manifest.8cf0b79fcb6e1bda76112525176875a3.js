ab3e84d2366c1b892ce5677b04592ae0b02e0235
kikoxxxi <17816853820@163.com>
2019-10-24 11:58:17 +0800

    build

---
+++ build/precache-manifest.8cf0b79fcb6e1bda76112525176875a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5c9badfa200ce2c93e68da8b895607",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ea86171658395d85a63",
+    "url": "./static/js/2.8d3b089c.chunk.js"
+  },
+  {
+    "revision": "bd2a9269423fc4193f70",
+    "url": "./static/js/3.f3c307ea.chunk.js"
+  },
+  {
+    "revision": "3007a6984e14420d40cf",
+    "url": "./static/js/main.6981d71f.chunk.js"
+  },
+  {
+    "revision": "6db1a272cd9380f9d7b7",
+    "url": "./static/js/runtime-main.e5291ac1.js"
+  }
+]);
